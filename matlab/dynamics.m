f5ff7215a68f9bba898b05bae0156e606844c4be
Evan Pezent <epezent@rice.edu>
2020-05-22 17:10:00 -0500

    mistakes in dynamics

--- matlab/dynamics.m
+++ matlab/dynamics.m
@@ -1,7 +1,6 @@
 % Evan Pezent | evanpezent.com | epezent@rIc_e.edu
 % 05/22/2020
 
-
 %% Define Symbolic Symbols
 syms tau1 eta1 Jm1 q1 q1d q1dd b1 fk1 ...
      tau2 eta2 Jm2 q2 q2d q2dd b2 fk2 ...
@@ -13,33 +12,41 @@ syms tau1 eta1 Jm1 q1 q1d q1dd b1 fk1 ...
      m_df Pc_dfx Pc_dfy Pc_dfz   Ic_dfxx Ic_dfxy Ic_dfxz Ic_dfyy Ic_dfyz Ic_dfzz ...
      m_g  Pc_gx  Pc_gy  Pc_gz    Ic_gxx Ic_gxy   Ic_gxz  Ic_gyy  Ic_gyz  Ic_gzz ...
      g
- 
-Tau = [tau1; tau2; tau3];
-Eta = [eta1;eta2;eta3];
-Jm = [Jm1;Jm2;Jm3];
 
+% joint torques 
+Tau = [tau1; tau2; tau3];
+% transmission ratios
+Eta = [eta1;eta2;eta3];
+% motor inertias
+Jm  = [Jm1;Jm2;Jm3];
+
+% joint angles
 Q = [q1;q2;q3];
+% joint velocities
 Qd = [q1d;q2d;q3d];
+% joint accelerations
 Qdd = [q1dd;q2dd;q3dd];
 
+% joint damping
 B = [b1;b2;b3];
+% joint kinetic friction
 Fk = [fk1;fk2;fk3];
 
+% rigidbody centers of mass
 Pc_a  =  [Pc_ax Pc_ay Pc_az].';
 Pc_c  =  [Pc_cx Pc_cy Pc_cz].';
 Pc_be = [Pc_bex Pc_bey Pc_bez].';
 Pc_df = [Pc_dfx Pc_dfy Pc_dfz].';
 Pc_g = [Pc_gx Pc_gy Pc_gz].';
 
-% TODO: zero non-diagonal
-Ic_a = [Ic_axx -Ic_axy -Ic_axz;
-       -Ic_axy  Ic_ayy -Ic_ayz;
-       -Ic_axz -Ic_ayz  Ic_axx];
+% rigidbody inertia tensors
+Ic_a  = [Ic_axx -Ic_axy -Ic_axz;
+        -Ic_axy  Ic_ayy -Ic_ayz;
+        -Ic_axz -Ic_ayz  Ic_axx];
    
-% TODO: zero non-diagonal   
-Ic_c = [Ic_cxx -Ic_cxy -Ic_cxz;
-       -Ic_cxy  Ic_cyy -Ic_cyz;
-       -Ic_cxz -Ic_cyz  Ic_cxx];
+Ic_c  = [Ic_cxx -Ic_cxy -Ic_cxz;
+        -Ic_cxy  Ic_cyy -Ic_cyz;
+        -Ic_cxz -Ic_cyz  Ic_cxx];
    
 Ic_be = [Ic_bexx -Ic_bexy -Ic_bexz;
         -Ic_bexy  Ic_beyy -Ic_beyz;
@@ -49,37 +56,39 @@ Ic_df = [Ic_dfxx -Ic_dfxy -Ic_dfxz;
         -Ic_dfxy  Ic_dfyy -Ic_dfyz;
         -Ic_dfxz -Ic_dfyz  Ic_dfxx];   
 
-Ic_g = [Ic_gxx -Ic_gxy -Ic_gxz;
-       -Ic_gxy  Ic_gyy -Ic_gyz;
-       -Ic_gxz -Ic_gyz  Ic_gxx];
-   
+Ic_g  = [Ic_gxx -Ic_gxy -Ic_gxz;
+        -Ic_gxy  Ic_gyy -Ic_gyz;
+        -Ic_gxz -Ic_gyz  Ic_gxx];
+    
+%% Forward Kinematics   
+
+% chain 1
 DH_table1 = [0 0 0 q1;
              0 pi/2 0 q2;
              l2 0 0 q3-q2];
-         
+[~,T_array1] = dh2tf(DH_table1);
+
+% chain 2       
 DH_table2 = [0 0 0 q1;
              0 pi/2 0 q3-pi/2
              l3 0 0 q2-q3];
-             
 [~,T_array2] = dh2tf(DH_table2);
-
+            
 g0 = [0; 0; g];
 
 %% Lagrangian Dynamics
 
 % setup Lagrangian first chain
-[~,T_array1] = dh2tf(DH_table1);
-m = [m_g,m_be,m_a];
-Pc = {Pc_g,Pc_be,Pc_a};
-Ic = {Ic_g,Ic_be,Ic_a};
-[~, ~, ~, ~, k1, u1] = dynamics_lagrangian(m,Pc,Ic,T_array1,Q,Qd,Qdd,g0,0);
+m1 = [m_g,m_be,m_a];
+Pc1 = {Pc_g,Pc_be,Pc_a};
+Ic1 = {Ic_g,Ic_be,Ic_a};
+[~, ~, ~, ~, k1, u1] = dynamics_lagrangian(m1,Pc1,Ic1,T_array1,Q,Qd,Qdd,g0,0);
 
 % setup Lagrangian second chain
-[~,T_array1] = dh2tf(DH_table2);
-m = [m_g,m_df,m_c];
-Pc = {Pc_g,Pc_df,Pc_c};
-Ic = {Ic_g,Ic_df,Ic_c};
-[~, ~, ~, ~, k2, u2] = dynamics_lagrangian(m,Pc,Ic,T_array1,Q,Qd,Qdd,g0,0);
+m2 = [m_g,m_df,m_c];
+Pc2 = {Pc_g,Pc_df,Pc_c};
+Ic2 = {Ic_g,Ic_df,Ic_c};
+[~, ~, ~, ~, k2, u2] = dynamics_lagrangian(m1,Pc1,Ic2,T_array2,Q,Qd,Qdd,g0,0);
 
 % sum kinetic/potential energies (k2(1) = k1(1) & u2(1) = u1(1), so skip)
 K = simplify( k1(1) + k1(2) + k1(3) + k2(2) + k2(3) ); 
@@ -102,6 +111,7 @@ end
 % compute torques / equations of motion
 MVG = simplify(ddtdLdQd - dLdQ); % 6.76
 
+%% Separate MVG into M, V, and G
 [M,V,G] = separate_mvg(MVG,Qdd,g);
 
 %% Get Equation of Motion
