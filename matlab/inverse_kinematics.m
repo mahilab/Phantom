147d68ae6ad7af686def194d8b3b0f8b27c8dc29
nathandunk <nbd2@rice.edu>
2020-05-25 16:33:56 -0500

    somehow switched names of forward and inverse kinematics files. Fixing that

--- matlab/inverse_kinematics.m
+++ matlab/inverse_kinematics.m
@@ -1,18 +1,18 @@
-% This function takes as its input a vector joint_angles of the form  
-% [theta1, theta2 , theta3 ] describing the position of each link relative 
-% to the previous one, and a vector gimbal_angles of roll-pitch-yaw angles, and outputs a
-% 4x4 homogeneous transformation phantom_T_0_g from the base frame to the
-% gimbal frame and a cell array phantom_T of the 4x4 homogeneous
-% transformations for this robot
+% This code generates the inverse kinematics of the phantom given an input
+% of x_ee, y_ee, z_ee
 
-syms l1 l2 theta1 theta2 theta3
+syms l1 l2 x_ee y_ee z_ee
 
-alpha = [     0,   pi/2,             0, pi/2].';
-a     = [     0,      0,            l1,    0].';
-d     = [     0,      0,             0,   l2].';
-theta = [theta1, theta2, theta3-theta2,    0].';
+l_star = sqrt(x_ee^2 + y_ee^2);
+ee_theta = atan2(z_ee,l_star);
+lh = sqrt(z_ee^2 + l_star^2);
 
-dh = [a alpha d theta];
+%Sol 1
+theta1(1) = atan2(y_ee,x_ee);
+theta2(1) = acos((l2^2-l1^2-lh^2)/(-2*l1*lh))+ee_theta;
+theta3(1) = acos((lh^2-l1^2-l2^2)/(-2*l1*l2))+theta2(1)-pi/2;
 
-[phantom_T0N,~,~] = dh2tf(dh);
-phantom_T0N(1:3,4)
+%Sol 2
+theta1(2) = wrapToPi(atan2(y_ee,x_ee)-pi);
+theta2(2) = pi-ee_theta+acos((l2^2-l1^2-lh^2)/(-2*l1*lh));
+theta3(2) = acos((lh^2-l1^2-l2^2)/(-2*l1*l2))+theta2(2)-pi/2;
